aaab5007c9066af07377b6d218b63320cbdd90a3
parthsarthi30 <parthdalai30@gmail.com>
2024-10-25 12:41:25 +0530

    import product from product.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
 import{cart} from "../data/cart.js";
+import { products} from "../data/products.js";
 
 let productsHTML = ''; //sabke liye ek html generate hoga iske pehle console me har product ke liye pura alag html generate hora tha
 //line 32 ki wajah se teeno ek saath combine hojaayenge
