9da6ed0b558aeef4cc6c3ce5a955c74081b83450
parthsarthi30 <parthdalai30@gmail.com>
2025-02-02 01:21:20 +0530

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,20 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+  cart= [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
+    quantity: 2,
 },
 {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1
 }];
+}
+
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addtoCart(productId){
     let matchingItem;
@@ -25,6 +34,7 @@ export function addtoCart(productId){
         quantity: 1
       });
     };
+    saveToStorage();
 }
 
 export function removefromcart(productId){
@@ -37,5 +47,7 @@ export function removefromcart(productId){
   });
 
   cart = newcart;
+
+  saveToStorage();
 
 }
